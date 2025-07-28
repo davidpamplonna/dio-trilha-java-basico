9274458e9fdda292795c6c14634ab5b52b10fc9f
David Pamplona <davidpamplonna@gmail.com>
2025-07-28 16:03:52 -0300

    Finalizando projeto de conta-banco

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, David!");
+        
+        Scanner scanner =  new Scanner(System.in);
+
+        // nome do cliente
+        System.out.println("Digite seu nome completo");
+        String name = scanner.next();
+        // número da conta
+        System.out.println("Digite o número da conta");
+        int conta = scanner.nextInt();
+        // número da agência
+        System.out.println("Digite o número da agência");
+        String agencia = scanner.next();
+        // saldo na conta
+        System.out.println("Digite o saldo da conta");
+        double saldo = scanner.nextDouble();
+
+
+
+        System.out.println("Olá %/, obrigado por criar uma conta em nosso, banco, sua agência é %/, conta %/ e seu saldo %/ já está disponível para saque");
+
+    
     }
 }
